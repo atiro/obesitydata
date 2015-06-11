20f1fb58de659a37c42a646771015510ea332108
Richard Palmer <richard@psyche.home>
2015-06-12 00:39:37 +0100

    Almost got first nvd3 graphing going.

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -8,5 +8,6 @@ from . import views
 # place app url patterns here
 
 urlpatterns = [
+        url(r'^annual/england$', views.annual_england),
         url(r'^annual/england/(?P<year>\d{4})$', views.annual_england)
 ]
--- diagnosis/views.py
+++ diagnosis/views.py
@@ -2,18 +2,32 @@ from django.shortcuts import render_to_response
 
 from diagnosis.models import Admissions
 
+
 def annual_england(request, year=None):
 
-    if year not None:
-       admissions = Admissions.objects.all().filter(year=year)
+    if year is not None:
+        admissions = Admissions.objects.all().filter(year=year)
 
-        return render_to_response('diagnosis/annual.html', {'admissions': admissions})
+        return render_to_response('diagnosis/annual.html',
+                                  {'admissions': admissions})
     else:
         admissions = Admissions.objects.all()
 
+        chartdata = {
+            'x': Admissions.objects.values_list('year', flat=True).distinct().order_by('year'),
+            'name1': 'Male',
+            'y1': Admissions.objects.filter(gender='M').values_list('admissions', flat=True).order_by('year'),
+            'name2': 'Female',
+            'y2': Admissions.objects.filter(gender='F').values_list('admissions', flat=True).order_by('year'),
+            'name3': 'Unknown',
+            'y2': Admissions.objects.filter(gender='U').values_list('admissions', flat=True).order_by('year'),
+        }
+
+        charttype = 'stackedAreaChart'
         data = {
-                'x': Admissions.objects.all().aggregate(year),
-                'name1': 'Male', 'y1': 
+            'charttype': charttype,
+            'chartdata': chartdata
+        }
 
         return render_to_response('diagnosis/england.html', {'data': data})
 
