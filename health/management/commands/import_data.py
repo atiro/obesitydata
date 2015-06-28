52482f5c2704fec0178e3c71f8a6416133b5d269
Richard Palmer <git@tiro.org.uk>
2015-06-28 23:15:54 +0100

    First health data importer (BMI)

---
+++ health/management/commands/import_data.py
@@ -0,0 +1,82 @@
+import csv
+from health.models import HealthWeight, HealthBMI
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Import HSE Health stats"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--hse_bmi', nargs=1, required=False)
+        parser.add_argument('--hse_weight', nargs=1, required=False)
+
+    def handle(self, *args, **options):
+
+        if options['hse_bmi']:
+            csvfilename = options['hse_bmi'][0]
+
+            if csvfilename.endswith(".csv"):
+                with open(csvfilename, 'rb') as csvfile:
+                    statsreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                    headers = statsreader.next()
+                    for statsline in statsreader:
+                        if statsline[0] == 'M':
+                            gender = HealthBMI.MALE
+                        elif statsline[0] == 'F':
+                            gender = HealthBMI.FEMALE
+                        elif statsline[0] == 'A':
+                            gender = HealthBMI.ALL
+                        else:
+                            print("PARSE ERROR - UNKNOWN GENDER")
+                            exit(1)
+
+                        if statsline[1] == '16-24':
+                            age = HealthBMI.AGE_16_TO_24
+                        elif statsline[1] == '25-34':
+                            age = HealthBMI.AGE_25_TO_34
+                        elif statsline[1] == '35-44':
+                            age = HealthBMI.AGE_35_TO_44
+                        elif statsline[1] == '45-54':
+                            age = HealthBMI.AGE_45_TO_54
+                        elif statsline[1] == '55-64':
+                            age = HealthBMI.AGE_55_TO_64
+                        elif statsline[1] == '65-74':
+                            age = HealthBMI.AGE_65_TO_74
+                        elif statsline[1] == '75+':
+                            age = HealthBMI.AGE_75_PLUS
+                        elif statsline[1] == 'All':
+                            age = HealthBMI.AGE_ALL
+                        else:
+                            print("PARSE ERROR - Unknown age")
+                            exit(1)
+
+                        if statsline[2] == 'Underweight':
+                            bmi = HealthBMI.BMI_UNDERWEIGHT
+                        elif statsline[2] == 'Normal':
+                            bmi = HealthBMI.BMI_NORMAL
+                        elif statsline[2] == 'Overweight':
+                            bmi = HealthBMI.BMI_OVERWEIGHT
+                        elif statsline[2] == 'Obese':
+                            bmi = HealthBMI.BMI_OBESE
+                        elif statsline[2] == 'Morbidly obese':
+                            bmi = HealthBMI.BMI_MORBIDLY_OBESE
+                        elif statsline[2] == 'Overweight including obese':
+                            bmi = HealthBMI.BMI_OVERWEIGHT_OBESE
+                        elif statsline[2] == 'Mean':
+                            bmi = HealthBMI.BMI_MEAN
+                        elif statsline[2] == 'Standard error of the mean':
+                            bmi = HealthBMI.BMI_STDERR
+                        elif statsline[2] == 'Base':
+                            bmi = HealthBMI.BMI_BASE
+                        else:
+                            print("PARSE ERROR - Unknown BMI")
+                            exit(1)
+
+                        year = 1993
+                        for year_val in statsline[3:]:
+                            if year_val == '-':
+                                year_val = 0
+                            health = HealthBMI(year=year, gender=gender, age=age, bmi=bmi, percentage=year_val)
+                            year += 1
+                            health.save()
--- health/models.py
+++ health/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
+
+class HealthWeight(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    ALL = 'A'
+
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (ALL, 'All')
+    )
+
+    year = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MALE)
+
+    weight_mean = models.FloatField()
+    weight_stderr = models.FloatField()
+    base = models.IntegerField()
+
+
+class HealthBMI(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    ALL = 'A'
+
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (ALL, 'All')
+    )
+
+    AGE_16_TO_24 = '16-24'
+    AGE_25_TO_34 = '25-34'
+    AGE_35_TO_44 = '35-44'
+    AGE_45_TO_54 = '45-54'
+    AGE_55_TO_64 = '55-64'
+    AGE_65_TO_74 = '65-74'
+    AGE_75_PLUS = '75+'
+    AGE_ALL = 'ALL'
+
+    AGE_CHOICES = (
+        (AGE_16_TO_24, '16-24'),
+        (AGE_25_TO_34, '25-34'),
+        (AGE_35_TO_44, '35-44'),
+        (AGE_45_TO_54, '45-54'),
+        (AGE_55_TO_64, '55-64'),
+        (AGE_65_TO_74, '65-74'),
+        (AGE_75_PLUS, '75+'),
+        (AGE_ALL, 'All Ages'),
+    )
+
+    BMI_UNDERWEIGHT = 'U'
+    BMI_NORMAL = 'N'
+    BMI_OVERWEIGHT = 'O'
+    BMI_OBESE = 'B'
+    BMI_MORBIDLY_OBESE = 'M'
+    BMI_OVERWEIGHT_OBESE = 'W'
+    BMI_MEAN = 'E'
+    BMI_STDERR = 'S'
+    BMI_BASE = 'A'
+
+    BMI_CHOICES = (
+        (BMI_UNDERWEIGHT, 'Underweight'),
+        (BMI_NORMAL, 'Normal'),
+        (BMI_OVERWEIGHT, 'Overweight'),
+        (BMI_OBESE, 'Obese'),
+        (BMI_MORBIDLY_OBESE, 'Morbidly Obese'),
+        (BMI_OVERWEIGHT_OBESE, 'Overweight including obese'),
+        (BMI_MEAN, 'Mean'),
+        (BMI_STDERR, 'Std error of the mean'),
+        (BMI_BASE, 'Base')
+    )
+
+    year = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MALE)
+    age = models.CharField(max_length=1, choices=AGE_CHOICES, default=AGE_16_TO_24)
+    bmi = models.CharField(max_length=1, choices=BMI_CHOICES, default=BMI_NORMAL)
+
 # Create your models here.
