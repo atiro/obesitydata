eec9581750cf064f83131d1782f3a63fcec924e2
Richard Palmer <git@tiro.org.uk>
2015-07-05 23:47:48 +0100

    Initial import for OS Code Point open data

---
+++ codepoint/management/commands/import_codepoint.py
@@ -0,0 +1,30 @@
+import csv
+import os
+from codepoint.models import CodePoint
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Import OS Code Point Open data"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--datadir', nargs=1, required=False)
+
+    def handle(self, *args, **options):
+
+        if options['datadir']:
+            datadir = options['datadir'][0]
+
+        for root, dir, files in os.walk(datadir):
+            for file in files:
+                if file.endswith(".csv"):
+                    with open(file, 'r') as csvfile:
+                        cpreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                        for cpline in cpreader:
+                            if len(cpline) != 10:
+                                print("Unknown line (skipped): %s" % cpline)
+                                next
+
+                            codepoint = CodePoint(postcode=cpline[0], pos_quality=cpline[1], eastings=cpline[2], northings=cpline[3], country_code=cpline[4], nhs_regional_ha_code=cpline[5], nhs_ha_code=cpline[6], admin_county_code=cpline[7], admin_district_code=cpline[8], admin_ward_code=cpline[9])
+                            codepoint.save()
--- codepoint/models.py
+++ codepoint/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CodePoint(models.Model):
+
+    postcode = models.CharField(max_length=7)
+    pos_quality = models.IntegerField()
+    eastings = models.IntegerField()
+    northings = models.IntegerField()
+    country_code = models.CharField(max_length=9)
+    nhs_regional_ha_code = models.CharField(max_length=9)
+    nhs_ha_code = models.CharField(max_length=9)
+    admin_county_code = models.CharField(max_length=9)
+    admin_district_code = models.CharField(max_length=9)
+    admin_ward_code = models.CharField(max_length=9)
